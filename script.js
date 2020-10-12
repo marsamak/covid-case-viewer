b6af64ca8c9ed453a3c1b7cf74418bc9b14edb85
marsamak <maria.samakhova@gmail.com>
2020-10-12 23:02:48 +0200

    Fill the table iterating throw a dictionary, as suggested by oktupol

--- script.js
+++ script.js
@@ -9,9 +9,15 @@ function getCurrentDate() {
 }
 var publikationsDatum = getCurrentDate(); //setting a current date
 
-//let summaryTableData = {}; //creating a dictionary
+function fillTableFromDict(summaryTableData) {
+  for (let elementId in summaryTableData) {
+    const value = summaryTableData[elementId];
+    document.getElementById(elementId).innerText = value;
+  }
+}
 
-function updateSummaryTable() { //here we fill our summaryTableData with numbers
+function updateSummaryTable() {
+  //here we fill our summaryTableData with numbers
   console.log("in updateNumbers()");
 
   var requestOptions = {
@@ -24,25 +30,19 @@ function updateSummaryTable() { //here we fill our summaryTableData with numbers
   fetch(summaryRequest, requestOptions)
     .then((response) => response.json())
     .then((result) => {
-        //console.log(result);
-       
-        //is there a better way to fill the summary table???
-        document.getElementById("casesWorldwide").innerText = result.Global.TotalConfirmed.toLocaleString();
-        document.getElementById("deathsWorldwide").innerText = result.Global.TotalDeaths.toLocaleString();
-        document.getElementById("casesEU").innerText = "??";
-        document.getElementById("deathsEU").innerText = "??";
-        document.getElementById("casesGermany").innerText = result.Countries[63].TotalConfirmed.toLocaleString();
-        document.getElementById("deathsGermany").innerText = result.Countries[63].TotalDeaths.toLocaleString();
-        document.getElementById("casesBavaria").innerText = Number(0).toLocaleString();
-        document.getElementById("deathsBavaria").innerText = Number(0).toLocaleString();
-        // summaryTableData.casesWorldwide = result.Global.TotalConfirmed.toLocaleString();
-        // summaryTableData.deathsWorldwide = result.Global.TotalDeaths.toLocaleString();
-        // summaryTableData.casesEU = "??";
-        // summaryTableData.deathsEU = "??";
-        // summaryTableData.casesGermany = result.Countries[63].TotalConfirmed.toLocaleString();
-        // summaryTableData.deathsGermany = result.Countries[63].TotalDeaths.toLocaleString();
-        // summaryTableData.casesBavaria = 0;
-        // summaryTableData.deathsBavaria = 0;
+      //console.log(result);
+
+      let summaryTableData = {}; //creating a dictionary
+      summaryTableData.casesWorldwide = result.Global.TotalConfirmed.toLocaleString(); 
+      summaryTableData.deathsWorldwide = result.Global.TotalDeaths.toLocaleString();
+      summaryTableData.casesEU = "??";
+      summaryTableData.deathsEU = "??";
+      summaryTableData.casesGermany = result.Countries[63].TotalConfirmed.toLocaleString();
+      summaryTableData.deathsGermany = result.Countries[63].TotalDeaths.toLocaleString();
+      summaryTableData.casesBavaria =  Number(0).toLocaleString();
+      summaryTableData.deathsBavaria =  Number(0).toLocaleString();
+
+      fillTableFromDict(summaryTableData);
     })
     .catch((error) => console.log("error", error));
 }
